c1c227ff1ef9c13bb7a070729c69645880ac9e83
Arisandi <arisandii033@gmail.com>
2022-12-15 23:22:20 +0800

    Membuat class aksi dan method main

---
+++ src/project02/RectangleAksi.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project02;
+
+/**
+ *
+ * @author warik
+ */
+public class RectangleAksi {
+    public static void main(String[] args) {
+        
+    }
+}
